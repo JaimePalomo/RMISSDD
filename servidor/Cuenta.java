5cef265971d3a979fdeed916f986980d46231f22
felbuecar <61271319+felbuecar@users.noreply.github.com>
2020-04-07 12:06:41 +0200

    Delete Cuenta.java

--- servidor/Cuenta.java
+++
@@ -1,8 +0,0 @@
-
-import java.rmi.*;
-
-interface Cuenta extends Remote {
-    String obtenerNombre() throws RemoteException;
-    float obtenerSaldo() throws RemoteException;
-    float operacion(float valor) throws RemoteException;
-}
