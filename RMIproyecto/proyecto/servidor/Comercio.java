182ca488db79ef894484220f5aaea95dddfb1698
felbuecar <61271319+felbuecar@users.noreply.github.com>
2020-04-09 14:27:29 +0200

    Delete Comercio.java

--- RMIproyecto/proyecto/servidor/Comercio.java
+++
@@ -1,7 +0,0 @@
-
-import java.rmi.*;
-
-interface Comercio extends Remote {
-    Comercio crearUsuario(String nombre, String correo, String contraseña) throws RemoteException;
-	Comercio comprobarUsuario(String nombre) throws RemoteException;
-}
