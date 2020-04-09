c0f2804db2ac28c8ed6e85911e35684f5f6ff44e
Felipe Bueno Carranza <felipebuenocarranza@gmail.com>
2020-04-09 14:01:34 +0200

    Jueves santo mañana

--- RMIproyecto/ejemplo_base_banco/4_banco_simple/servidor/Banco.java
+++
@@ -1,6 +0,0 @@
-
-import java.rmi.*;
-
-interface Banco extends Remote {
-    Cuenta crearCuenta(String nombre) throws RemoteException;
-}
--- RMIproyecto/proyecto/servidor/Usuario.java
+++ RMIproyecto/proyecto/servidor/Usuario.java
@@ -1,8 +1,33 @@
 
-import java.rmi.*;
+import java.io.*;
+import java.lang.*;
 
-interface Usuario extends Remote {
-    String obtenerNombre() throws RemoteException;
-    float obtenerSaldo() throws RemoteException;
-    float operacion(float valor) throws RemoteException;
+class Usuario implements Serializable {
+     private String nombre;
+     private String contraseña;
+     private String saldo; 
+	public Usuario(String nombre, String contraseña, String saldo)
+	{
+		this.nombre=nombre;
+		this.contraseña=contraseña;
+		this.saldo=saldo;
+	}
+     	public String obtenerSaldo() {
+        	 return this.saldo;
+     	}
+	public void cambiarContraseña(String nueva) {
+		this.contraseña = nueva;
+		
+	}
+	public String añadirSaldo(float valor) {
+		float saldo_actual=valueOf(this.saldo);
+        	this.saldo=toString(saldo_actual + valor);
+        	return this.saldo;
+    	}
+        public void RealizarPedido(int listaProductos[], List <Producto> catalogo){
+
+	
+
+	
+	}
 }
