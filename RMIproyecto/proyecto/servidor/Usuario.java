8eea54cf4382b4e256dab25174d81d14ca14a75f
felbuecar <61271319+felbuecar@users.noreply.github.com>
2020-04-09 14:28:44 +0200

    Add files via upload

--- RMIproyecto/proyecto/servidor/Usuario.java
+++ RMIproyecto/proyecto/servidor/Usuario.java
@@ -1,8 +1,37 @@
-//fichero de usuario 
-import java.rmi.*;
 
-interface Usuario extends Remote {
-    String obtenerNombre() throws RemoteException;
-    float obtenerSaldo() throws RemoteException;
-    float operacion(float valor) throws RemoteException;
+import java.io.*;
+import java.lang.*;
+
+class Usuario implements Serializable {
+     private String nombre;
+     private String contraseña;
+     private String saldo; 
+     private String direccion;
+	public Usuario(String nombre, String contraseña, String saldo, String direccion)
+	{
+		this.nombre=nombre;
+		this.contraseña=contraseña;
+		this.saldo=saldo;
+		this.direccion=direccion
+	}
+     	public String obtenerSaldo() {
+        	 return this.saldo;
+     	}
+	public void cambiarContraseña(String nueva) {
+		this.contraseña = nueva;	
+	}
+	public void cambiarDireccion(String nueva) {
+		this.direccion = nueva;	
+	}
+	public String añadirSaldo(float valor) {
+		float saldo_actual=valueOf(this.saldo);
+        	this.saldo=toString(saldo_actual + valor);
+        	return this.saldo;
+    	}
+        public void RealizarPedido(int listaProductos[], List <Producto> catalogo){
+
+	
+
+	
+	}
 }
