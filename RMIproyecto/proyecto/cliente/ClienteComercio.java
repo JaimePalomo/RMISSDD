edf20e045196ff3f088bef701fa22b2058f9c235
JaimePalomo <palomosivianesjaime@gmail.com>
2020-04-09 15:08:38 +0200

    Completando cliente

--- RMIproyecto/proyecto/cliente/ClienteComercio.java
+++ RMIproyecto/proyecto/cliente/ClienteComercio.java
@@ -2,6 +2,7 @@
 import java.util.*;
 import java.rmi.*;
 import java.rmi.server.*;
+import java.lang.*;
 
 
 class ClienteComercio {
@@ -10,13 +11,14 @@ class ClienteComercio {
 	public static int MOSTRAR_CATÁLOGO = 2;
 	public static int REALIZAR_PEDIDO = 3;
 	public static int MODIFICAR_DATOS_USUARIO = 4;
-	public static int SALIR = 5;
+	public static int AÑADIR_SALDO = 5;
+	public static int SALIR = 6;
 	    
 	int flag = 1; //Bandera para la salida del menu
 	List <Producto> catalogo; //Lista en la que guardaremos los objetos de la clase Producto
 	
-        if (args.length!=3) {
-            System.err.println("Uso: ClienteComercio hostregistro numPuertoRegistro Usuario");
+        if (args.length!=4) {
+            System.err.println("Uso: ClienteComercio hostregistro numPuertoRegistro Usuario Contraseña");
             return;
         }
 
@@ -25,8 +27,9 @@ class ClienteComercio {
 
         try {
             OrderSL srv = (OrderSL) Naming.lookup("//" + args[0] + ":" + args[1] + "/OrderSL");
-            Usuario u = srv.crearUsuario(args[2]); //Obtenemos el usuario que tiene el nombre introducido por el cliente
-	    catalogo=srv.ObtenerCatalogo(); //La función ObtenerCatalogo() devolvera una lista de clase Producto con todos los productos disponibles 
+	    
+            Usuario u = srv.crearUsuario(args[2], args[3]); //Obtenemos el usuario que tiene el nombre introducido por el cliente
+	    catalogo=srv.obtenerCatalogo(); //La función ObtenerCatalogo() devolvera una lista de clase Producto con todos los productos disponibles 
 	    while( flag == 1){ //El bucle se seguirá realizando e imprimiendo el menú hasta que el cliente quiera salir
 		FileOutputStream os = new FileOutputStream("Menu.txt");
 		PrintStream ps = new PrintStream(os); //Imprimimos el menú
@@ -34,7 +37,7 @@ class ClienteComercio {
 		int i = sc.nextInt();
 		switch (i){
 		case COMPROBAR_PEDIDOS:
-		    ComprobarPedidos(u); 
+		    comprobarPedidos(u); 
 		    break;
 		case MOSTRAR_CATÁLOGO:
 		  
@@ -42,10 +45,13 @@ class ClienteComercio {
 			System.out.println(z.getNombre() + "	precio: " + z.getPrecio() +"euros	id: " z.getId() "\n" ); //Imprimimos el catálago
 		    break;
 		case REALIZAR_PEDIDO:
-		    RealizarPedido(u);
+		    realizarPedido(u, catalogo);
 		    break;
 		case MODIFICAR_DATOS_USUARIO:
 		    
+		    break;
+		case AÑADIR_SALDO:
+		    añadirSaldo(u);
 		    break;
 		case SALIR:
 		    flag = 0;
@@ -66,14 +72,14 @@ class ClienteComercio {
         }
     }
 	
-    public void ComprobarPedidos(Usuario u){ //Esta función llamará al servidor para que nos devuelva todos los pedidos que tenga el usuario
+    public void comprobarPedidos(Usuario u){ //Esta función llamará al servidor para que nos devuelva todos los pedidos que tenga el usuario
 	List <Pedido> p;
-	p=u.ObtenerPedidos();
+	p=u.obtenerPedidos();
 	for (Pedido x : p){
 
 	}
     }
-    public void RealizarPedido(Usuario u){ //Esta función le pasará al servidor un array de int con las id de los productos que quiere pedir
+    public void realizarPedido(Usuario u, List <Producto> catalogo){ //Esta función le pasará al servidor un array de int con las id de los productos que quiere pedir
 	flag = 0;
 	int listaProductos[];
 	int aux=0;
@@ -88,8 +94,23 @@ class ClienteComercio {
 		break;
 	    default:
 		listaProductos[aux]=i;
-		aux++;		
+		aux++;
 	    }
 	}
-	u.RealizarPedido(listaProductos);
+	
+	u.realizarPedido(listaProductos, catalogo);
+    }
+
+    public void añadirSaldo(Usuario u){
+	String nuevoSaldo; 
+	System.out.println("Saldo actual: "+ u.obtenerSaldo() +"euros \n");
+	System.out.println("Saldo a añadir: ");
+	Scanner sc = new Scanner(System.in);
+	float aux = sc.nextFloat();
+	nuevoSaldo=u.añadirSaldo(aux);
+	System.out.println(" \n Nuevo saldo: "+ nuevoSaldo+"euros \n");
+
+    }
 }
+
+    
